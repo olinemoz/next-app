134f2caa0ecab50827c357c26c164c0d3ae4144f
olinemoz <oline@appstle.com>
2023-06-23 19:12:23 +0600

    added dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import {Inter} from 'next/font/google'
 import React from "react";
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
+import ThemeContextProvider from "@/context/ThemeContext";
 
 const inter = Inter({subsets: ['latin']})
 
@@ -15,11 +16,13 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
         <html lang="en">
         <body className={inter.className}>
+        <ThemeContextProvider>
             <div className="container">
                 <Navbar/>
                 {children}
                 <Footer/>
             </div>
+        </ThemeContextProvider>
         </body>
         </html>
     )
